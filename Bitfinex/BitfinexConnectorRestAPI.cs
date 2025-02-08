6506beaef9bcf309187fc634b64deb33e4c94d47
Tihohod2000 <dumanovskiy15@gmail.com>
2025-02-08 13:47:50 +0300

    GetNewTrades

--- Bitfinex/BitfinexConnectorRestAPI.cs
+++ Bitfinex/BitfinexConnectorRestAPI.cs
@@ -1,13 +1,46 @@
 ﻿using ConnectorTest;
 using TestHQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using RestSharp;
 
 namespace Bitfinex;
 
 public class BitfinexConnectorRestAPI : ITestConnector
 {
-    public Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount)
+    private readonly RestClient _client;
+    
+    public BitfinexConnectorRestAPI()
     {
-        throw new NotImplementedException();
+        var options = new RestClientOptions("https://api-pub.bitfinex.com/v2/");
+        _client = new RestClient(options);
+    }
+    
+    public async Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount)
+    {
+        var request = new RestRequest($"trades/t{pair}/hist?limit={maxCount}&sort=-1");
+        request.AddHeader("accept", "application/json");
+
+        var response = await _client.GetAsync(request);
+        if (!response.IsSuccessful || response.Content == null)
+        {
+            throw new Exception("Error request " + response.ErrorException);
+        }
+        
+        var trades = JsonSerializer.Deserialize<List<List<object>>>(response.Content);
+
+        return trades.Select(t => new Trade
+        {
+            Id = Convert.ToInt64(t[0]),
+            Pair = pair,
+            Time = DateTimeOffset.FromUnixTimeMilliseconds(Convert.ToInt64(t[1])),
+            Amount = Convert.ToDecimal(t[2]),
+            Price = Convert.ToDecimal(t[3])
+        });
     }
 
     public Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count)
