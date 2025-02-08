7752fbd709cd578c9b7673b6a3dfb7749e8ea5bf
Tihohod2000 <dumanovskiy15@gmail.com>
2025-02-08 13:19:29 +0300

    Update BitfinexConnectorRestAPI.cs

--- Bitfinex/BitfinexConnectorRestAPI.cs
+++ Bitfinex/BitfinexConnectorRestAPI.cs
@@ -1,9 +1,42 @@
 ﻿using ConnectorTest;
+using TestHQ;
 
 namespace Bitfinex;
 
 public class BitfinexConnectorRestAPI : ITestConnector
 {
-    
+    public Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public event Action<Trade>? NewBuyTrade;
+    public event Action<Trade>? NewSellTrade;
+    public void SubscribeTrades(string pair, int maxCount = 100)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UnsubscribeTrades(string pair)
+    {
+        throw new NotImplementedException();
+    }
+
+    public event Action<Candle>? CandleSeriesProcessing;
+
+    public void SubscribeCandles(string pair, int periodInSec, DateTimeOffset? from = null, DateTimeOffset? to = null,
+        long? count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UnsubscribeCandles(string pair)
+    {
+        throw new NotImplementedException();
+    }
 }
